15d725266e7685b1b16f3f26dd049213776c230c
wzh <78910421@qq.com>
2021-10-09 10:37:05 +0800

    1. 将system注册到eureka里面

---
+++ system/src/main/java/com/wzh/course/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.wzh.course.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "success";
+    }
+}
