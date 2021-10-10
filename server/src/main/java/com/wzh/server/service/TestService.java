1c8d37c5c70f304d08699c590dd4a368c8225d6f
wzh <78910421@qq.com>
2021-10-10 11:07:43 +0800

    1. Example使用示例 使用Example可以快速完成单表的增删改查，熟练掌握后对代码开发效率有一个质的飞跃

--- server/src/main/java/com/wzh/server/service/TestService.java
+++ server/src/main/java/com/wzh/server/service/TestService.java
@@ -1,11 +1,12 @@
 package com.wzh.server.service;
 
 import com.wzh.server.domain.Test;
+import com.wzh.server.domain.TestExample;
 import com.wzh.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class TestService {
@@ -13,8 +14,11 @@ public class TestService {
     @Resource
     private TestMapper testMapper;
 
-//    public List<Test> list() {
-//        return testMapper.list();
-//    }
+    public List<Test> list() {
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+    }
 
 }
